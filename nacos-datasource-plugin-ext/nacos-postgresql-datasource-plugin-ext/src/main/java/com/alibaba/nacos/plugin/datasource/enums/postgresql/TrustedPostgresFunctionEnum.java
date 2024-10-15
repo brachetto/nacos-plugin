58fdb99b5c93aea3872aef2d6d348ba519571f80
brachetto <41586583+brachetto@users.noreply.github.com>
2024-10-15 17:57:28 +0800

    1、配置列表查询增加未配置格式字段; 2、增加信任函数枚举

--- nacos-datasource-plugin-ext/nacos-postgresql-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/enums/postgresql/TrustedPostgresFunctionEnum.java
+++ nacos-datasource-plugin-ext/nacos-postgresql-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/enums/postgresql/TrustedPostgresFunctionEnum.java
@@ -1,22 +1,23 @@
-package com.alibaba.nacos.plugin.datasource.enums.oracle;
+package com.alibaba.nacos.plugin.datasource.enums.postgresql;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author brachetto
- * @description TODO
- * @createTime 2024-10-15 16:02
+ * @author xiang
+ * @createTime 2024-10-15 17:11
  * @updateTime
  */
-public enum TrustedOracleFunctionEnum {
+public enum TrustedPostgresFunctionEnum {
+    /**
+     * NOW().
+     */
+    NOW("NOW()", "CURRENT_TIMESTAMP");
 
-    NOW("NOW()", "TO_CHAR(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF3')");
-
-    private static final Map<String, TrustedOracleFunctionEnum> LOOKUP_MAP = new HashMap<>();
+    private static final Map<String, TrustedPostgresFunctionEnum> LOOKUP_MAP = new HashMap<>();
 
     static {
-        for (TrustedOracleFunctionEnum entry : TrustedOracleFunctionEnum.values()) {
+        for (TrustedPostgresFunctionEnum entry : TrustedPostgresFunctionEnum.values()) {
             LOOKUP_MAP.put(entry.functionName, entry);
         }
     }
@@ -25,7 +26,7 @@ public enum TrustedOracleFunctionEnum {
 
     private final String function;
 
-    TrustedOracleFunctionEnum(String functionName, String function) {
+    TrustedPostgresFunctionEnum(String functionName, String function) {
         this.functionName = functionName;
         this.function = function;
     }
@@ -37,7 +38,7 @@ public enum TrustedOracleFunctionEnum {
      * @return function
      */
     public static String getFunctionByName(String functionName) {
-        TrustedOracleFunctionEnum entry = LOOKUP_MAP.get(functionName);
+        TrustedPostgresFunctionEnum entry = LOOKUP_MAP.get(functionName);
         if (entry != null) {
             return entry.function;
         }
